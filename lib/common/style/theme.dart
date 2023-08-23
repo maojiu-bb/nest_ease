cc807a6852aa7b956c08a15f839a3ce2a020b317
maojiu <maojiu-bb@outlook.com>
2023-08-23 11:32:56 +0800

    feat -> 更改系统样式

--- lib/common/style/theme.dart
+++ lib/common/style/theme.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:nestease_cloud_music/common/index.dart';
 
 /// 主题
@@ -6,10 +7,18 @@ class AppTheme {
   // light
   static ThemeData light = ThemeData(
     colorScheme: lightColorScheme,
+    appBarTheme: const AppBarTheme(
+      // 与主题色相反
+      systemOverlayStyle: SystemUiOverlayStyle.dark,
+    ),
   );
 
   // dark
   static ThemeData dark = ThemeData(
     colorScheme: darkColorScheme,
+    appBarTheme: const AppBarTheme(
+      // 与主题色相反
+      systemOverlayStyle: SystemUiOverlayStyle.light,
+    ),
   );
 }

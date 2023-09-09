ff7a423f85c4f0cd053097cbca7a5a1c98c084c9
maojiu <maojiu-bb@outlook.com>
2023-09-09 11:41:05 +0800

    feat -> 添加歌单页面appear

--- lib/pages/music/music_all_list/view.dart
+++ lib/pages/music/music_all_list/view.dart
@@ -1,10 +1,35 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:nestease_cloud_music/common/index.dart';
 
 import 'index.dart';
 
 class MusicAllListPage extends GetView<MusicAllListController> {
   const MusicAllListPage({Key? key}) : super(key: key);
+
+  // appbar
+  AppBar _buildAppBar() {
+    return AppBar(
+      // 背景透明
+      backgroundColor: Colors.transparent,
+      // 取消阴影
+      elevation: 0,
+      // 标题栏左侧间距
+      titleSpacing: AppSpace.listItem,
+      leading: ButtonWidget.icon(
+        Icon(
+          Icons.arrow_back_ios,
+          color: AppColors.onBackground,
+        ),
+        onTap: () => Get.back(),
+      ),
+      title: TextWidget.title2(
+        '歌单分类',
+        color: AppColors.onBackground,
+      ),
+      centerTitle: true,
+    );
+  }
 
   // 主视图
   Widget _buildView() {
@@ -20,7 +45,7 @@ class MusicAllListPage extends GetView<MusicAllListController> {
       id: "music_all_list",
       builder: (_) {
         return Scaffold(
-          appBar: AppBar(title: const Text("music_all_list")),
+          appBar: _buildAppBar(),
           body: SafeArea(
             child: _buildView(),
           ),
